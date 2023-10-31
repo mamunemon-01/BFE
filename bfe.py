082dcdd3f652e16ac9cc0d0506fe499ae821143b
Mamun <sciemontist@gmail.com>
2023-10-31 21:31:35 +0600

    Added jpg again in the acceptd file type list

--- bfe.py
+++ bfe.py
@@ -12,7 +12,7 @@ st.markdown('<h1 style="color:blue;">A Machine Learning Approach to Recognize Ma
 st.markdown('<h2 style="color:gray;">Our model classifies facial expressions into the following categories:</h2>', unsafe_allow_html = True)
 st.markdown('<h3 style="color:gray;">Happiness, Sadnesss and Other</h3>', unsafe_allow_html = True)
 
-upload = st.file_uploader('Insert image for classification:', type = ['png'])#, 'jpg'])
+upload = st.file_uploader('Insert image for classification:', type = ['png', 'jpg'])
 #print(upload, end= '\n\n')
 
 #c1, c2 = st.columns(2)
